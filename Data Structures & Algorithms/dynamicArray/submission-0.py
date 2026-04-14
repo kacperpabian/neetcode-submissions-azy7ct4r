326738def9726ef90557c35c366763f2a99c75c3
Kacper Pabian <kacperpb96@gmail.com>
2026-04-14 11:29:09 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.py
@@ -0,0 +1,37 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.arr = [0] * capacity
+        self.capacity = capacity
+        self.size = 0
+
+    def get(self, i: int) -> int:
+        return self.arr[i]
+
+    def set(self, i: int, n: int) -> None:
+        self.arr[i] = n
+
+    def pushback(self, n: int) -> None:
+        if self.size == self.capacity:
+            self.resize()
+        self.arr[self.size] = n
+        self.arr.append(n)
+        self.size += 1
+
+    def popback(self) -> int:
+        self.size -= 1
+        return self.arr[self.size]
+
+    def resize(self) -> None:
+        new_capacity = self.capacity * 2
+        new_arr = [0] * new_capacity
+        for i in range(self.size):
+            new_arr[i] = self.arr[i]
+        self.arr = new_arr
+        self.capacity = new_capacity
+
+    def getSize(self) -> int:
+        return self.size
+    
+    def getCapacity(self) -> int:
+        return self.capacity
